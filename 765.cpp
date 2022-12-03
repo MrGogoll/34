a6a57df2cccf72828b3a2ed7e140bdaac2c8815e
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-12-03 15:26:33 +0300

    Add files via upload

---
+++ 765.cpp
@@ -0,0 +1,37 @@
+﻿using namespace std;
+#include <iostream>
+struct shk {
+	char name;
+	int kll;
+	char kllb;
+	float sroc;
+};
+
+int main()
+{
+
+	shk a;
+	a.name = 'ABBA';
+	a.kll = 4;
+	a.kllb = 'A';
+	a.sroc = 3.23;
+	shk b;
+	b.name = 'ASA';
+	b.kll = 8;
+	b.kllb = 'B';
+	b.sroc = 4.32;
+	shk c;
+	c.name = 'UWU';
+	c.kll = 9;
+	c.kllb = 'A';
+	c.sroc = 2.53;
+	if (a.sroc > b.sroc && a.sroc > c.sroc) {
+		cout << a.sroc;
+	}
+	if (c.sroc > b.sroc && c.sroc > a.sroc) {
+		cout << c.sroc;
+	}
+	if (b.sroc > c.sroc && b.sroc > a.sroc) {
+		cout << b.sroc;
+	}
+}
